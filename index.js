c1fb0c9349949bb503ba8c43ce5b7c6f0a3a74f4
docomo4163-star <docomo.4163@gmail.com>
2026-01-02 07:16:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ process.on('uncaughtException', (err) => {
 client.once('ready', (c) => {
   isReady = true;
   console.log(`✅ ${c.user.tag} でログイン中`);
-  c.user.setActivity('性的な人生3', { type: 0 });
+  c.user.setActivity('性的な人生0.4', { type: 0 });
 });
 
 // ---- メッセージ ----
